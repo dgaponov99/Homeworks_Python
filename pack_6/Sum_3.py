36cb0947300bff3a5aef678c4a8e23b5d32c77b9
dmitry <dgaponov99@mail.ru>
2017-12-02 05:19:27 +0300

    add Sum_3.py

---
+++ pack_6/Sum_3.py
@@ -0,0 +1,40 @@
+import random
+
+import Base
+
+l = Base.PositionalList()
+
+n = int(input('Введите количество элементов: '))
+
+i = 0
+while l.len() < n:
+    probability = random.randint(0, 1)
+    if probability:
+        l.add_last(i)
+    probability = random.randint(0, 100)
+    if probability > 20:
+        i += 1
+
+for i in l:
+    print(i, end=' ')
+print()
+
+v = int(input('Введите предполагаемую сумму: '))
+
+
+def search():
+    global l, v
+    a = l._header._next
+    b = l._trailer._prev
+
+    while a != b:
+        if a._element + b._element == v:
+            return a._element, b._element
+        elif a._element + b._element < v:
+            a = a._next
+        else:
+            b = b._prev
+    return None
+
+
+print(search())
